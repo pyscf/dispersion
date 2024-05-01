13cc4d23e1c13a14942837a5f1e3f4a90a416bc5
Qiming Sun <osirpt.sun@gmail.com>
2024-05-01 07:37:36 -0700

    Unittests (#1)
    * Add error handler and unittests
    
    * Missing dependency
    
    * Fix tests

---
+++ pyscf/dispersion/tests/test_d3.py
@@ -0,0 +1,22 @@
+import pytest
+import pyscf
+from pyscf.dispersion.dftd3 import DFTD3Dispersion
+
+def test_d3_unknown_xc():
+    mol = pyscf.M(atom='H 0 0 0; H 0 0 1')
+    with pytest.raises(RuntimeError):
+        model = DFTD3Dispersion(mol, xc='wb97x-v')
+
+def test_d3_energy():
+    mol = pyscf.M(atom='H 0 0 0; H 0 0 1')
+    model = DFTD3Dispersion(mol, xc='WB97X')
+    out = model.get_dispersion()
+    assert abs(out['energy'] - -0.000289774198733) < 1e-10
+
+def test_d3_gradients():
+    mol = pyscf.M(atom='H 0 0 0; H 0 0 1')
+    model = DFTD3Dispersion(mol, xc='HF')
+    out = model.get_dispersion(grad=True)
+    assert abs(out['energy'] - -0.002682864283734) < 1e-10
+    assert abs(out['gradient'][0, 2] - 0.0004553384490128) < 1e-10
+    assert abs(out['virial'][2, 2] - -0.000860464962618) < 1e-10
