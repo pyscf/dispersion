838706ef482415a52fbaacfac26d8f0180f43ac5
Xiaojie Wu <xiaojie.wu@bytedance.com>
2025-01-27 15:18:13 +0800

    ci

--- pyscf/dispersion/tests/test_d4.py
+++ pyscf/dispersion/tests/test_d4.py
@@ -1,37 +1,22 @@
+# Copyright 2024 The PySCF Developers. All Rights Reserved.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 import numpy as np
 import pytest
 import pyscf
 from pyscf.dispersion.dftd4 import DFTD4Dispersion
-
-vitamin = """
-C                 -0.07551087    1.68127663   -0.10745193
-O                  1.33621755    1.87147409   -0.39326987
-C                  1.67074668    2.95729545    0.49387976
-C                  0.41740763    3.77281969    0.78495878
-C                 -0.60481480    3.07572636    0.28906224
-H                 -0.19316298    1.01922455    0.72486113
-O                  0.35092043    5.03413298    1.45545728
-H                  0.42961487    5.74279041    0.81264173
-O                 -1.95331750    3.53349874    0.15912025
-H                 -2.55333895    2.78846397    0.23972698
-O                  2.81976302    3.20110148    0.94542226
-C                 -0.81772499    1.09230218   -1.32146482
-H                 -0.70955636    1.74951833   -2.15888136
-C                 -2.31163857    0.93420736   -0.98260166
-H                 -2.72575463    1.89080093   -0.74107186
-H                 -2.41980721    0.27699120   -0.14518512
-O                 -0.26428017   -0.18613595   -1.64425697
-H                 -0.72695910   -0.55328886   -2.40104423
-O                 -3.00083741    0.38730252   -2.10989934
-H                 -3.93210821    0.28874990   -1.89865997
-"""
-
-def test_d4():
-    mol = pyscf.M(atom=vitamin)
-    model = DFTD4Dispersion(mol, xc='r2scan3c')
-    out = model.get_dispersion()
-
-    print(out)
 
 def test_d4_unknown_xc():
     mol = pyscf.M(atom='H 0 0 0; H 0 0 1')
